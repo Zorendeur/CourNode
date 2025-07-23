a63c3d5b4371a08ea9f9fecc3b1537e0c6657d2a
JoshuaPR-dev <joshuaprv.contact@gmail.com>
2025-07-23 11:12:32 +0200

    Pour Bocar

--- models/articles.model.js
+++ models/articles.model.js
@@ -3,31 +3,31 @@ const prisma = require("../services/prismaClient");
 
 exports.getAllArticles = async () => {
   const articles = await prisma.articles.findMany({
-  where: {
-    titre: {
-      contains: "e"
-    }
-  },
-  orderBy: {
-    date_creation: "desc"
-  },
-  select: {
-    id: true,
-    titre: true,
-    contenu: true,
-    date_creation: true,
-    commentaires: {
-      select: {
-        contenu: true,
-        auteur: true
-      }
-    }
-  },
-  take: 3
-})
-console.log(articles);
-}
-
+    where: {
+      titre: {
+        contains: "e",
+        mode: "insensitive",
+      },
+    },
+    orderBy: {
+      date_creation: "desc",
+    },
+    select: {
+      id: true,
+      titre: true,
+      contenu: true,
+      date_creation: true,
+      commentaires: {
+        select: {
+          contenu: true,
+          auteur: true,
+        },
+      },
+    },
+    take: 3,
+  });
+  console.log(JSON.stringify(articles, (k, v) => (typeof v === "bigint" ? v.toString() : v), 2));
+};
 
 // exports.getArticleById = async (id) => {
 //   const { data, error } = await supabase.from('articles').select('*').eq('id', id).single();
